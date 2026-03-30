c830aea0827d17b36d02296b796ceb1c513018a3
Abdul Ahad <ammaar07@gmail.com>
2026-03-30 07:02:04 +0530

    add reset and input buttons

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const grid = 30;
+//Number
+const gridInput = document.querySelector("#gridInput");
+const resetBtn = document.querySelector("#resetBtn");
 
 //Container
 const container = document.querySelector("#container");
@@ -7,22 +9,43 @@ container.style.display = "flex";
 
 container.style.border = "1px solid black";
 
-// Grid
-for (i = 0; i < grid; i++) {
-  const gridRow = document.createElement("div");
+const buildGrid = () => {
+  container.innerHTML = "";
+  const grid = parseInt(gridInput.value);
+  for (i = 0; i < grid; i++) {
+    const gridRow = document.createElement("div");
 
-  gridRow.style.display = "flex";
-  gridRow.style.flexDirection = "column";
-  gridRow.style.flex = "1";
+    gridRow.style.display = "flex";
+    gridRow.style.flexDirection = "column";
+    gridRow.style.flex = "1";
 
-  for (j = 0; j < grid; j++) {
-    const gridColumn = document.createElement("div");
+    for (j = 0; j < grid; j++) {
+      const gridColumn = document.createElement("div");
 
-    gridColumn.style.flex = "1";
-    gridColumn.style.border = "1px solid black";
+      gridColumn.style.flex = "1";
+      gridColumn.style.border = "1px solid black";
 
-    gridRow.appendChild(gridColumn);
+      gridColumn.addEventListener("mouseenter", () => {
+        gridColumn.style.backgroundColor = "black";
+      });
+
+      gridRow.appendChild(gridColumn);
+    }
+
+    container.appendChild(gridRow);
   }
+};
 
-  container.appendChild(gridRow);
-}
+gridInput.addEventListener("input", () => {
+  if (gridInput.value > 100) gridInput.value = 100;
+  if (gridInput.value < 1) gridInput.value = 1;
+  buildGrid();
+});
+
+resetBtn.addEventListener("click", () => {
+  container
+    .querySelectorAll("div")
+    .forEach((cell) => (cell.style.backgroundColor = ""));
+});
+
+buildGrid();
